bc58397cdf7bcf093081d77751847c0c97833f9f
Suyesh Bhandari <suyeshb@gmail.com>
2019-03-21 18:21:04 -0400

    .

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,20 +1,48 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, Button, TouchableOpacity } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { MaterialIcons } from "@expo/vector-icons";
 import { TOP_MARGIN } from "../../constants";
 
 class TopBar extends Component {
+  state = {
+    firstQuery: ""
+  };
   render() {
+    const { firstQuery } = this.state;
     return (
       <View
         style={{
-          backgroundColor: "red",
           width: "100%",
           height: 60,
           position: "absolute",
           top: 0,
-          marginTop: TOP_MARGIN
+          marginTop: TOP_MARGIN,
+          flexDirection: "row",
+          justifyContent: "space-around",
+          alignItems: "center",
+          paddingHorizontal: 20
         }}
-      />
+      >
+        <TouchableOpacity>
+          <MaterialIcons name="menu" size={30} color="black" />
+        </TouchableOpacity>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={query => {
+            this.setState({ firstQuery: query });
+          }}
+          value={firstQuery}
+          style={{
+            flex: 1,
+            marginHorizontal: 10,
+            borderRadius: 50
+          }}
+        />
+        <TouchableOpacity>
+          <MaterialIcons name="my-location" size={30} color="black" />
+        </TouchableOpacity>
+      </View>
     );
   }
 }
