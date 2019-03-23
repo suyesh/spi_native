4c888605b6823b524457c2761b7887e4e6ec1466
Suyesh Bhandari <suyeshb@gmail.com>
2019-03-23 00:06:24 -0400

    ,

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { MainContainer } from "../components";
 import { Map, Header } from "../containers";
+import BottomSheet from "reanimated-bottom-sheet";
+import { Surface, Text } from "react-native-paper";
 
 const Main = () => (
   <MainContainer>
     <Map />
     <Header />
+    <BottomSheet
+      snapPoints={[600, 300, 50]}
+      renderContent={() => (
+        <Surface style={{ width: "100%", height: "100%" }}>
+          <Text>Hello</Text>
+        </Surface>
+      )}
+      renderHeader={() => (
+        <Surface style={{ width: "100%" }}>
+          <Text>Hello</Text>
+        </Surface>
+      )}
+      initialSnap={2}
+    />
   </MainContainer>
 );
 
