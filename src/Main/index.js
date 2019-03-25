a787a8ccca2850072e91fbefc0d3691fcb9514dd
Suyesh Bhandari <suyeshb@gmail.com>
2019-03-24 21:47:02 -0400

    .

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,27 +1,13 @@
 import React from "react";
 import { MainContainer } from "../components";
-import { Map, Header } from "../containers";
-import BottomSheet from "reanimated-bottom-sheet";
-import { Surface, Text } from "react-native-paper";
+import { Map, Header, BottomContent } from "../containers";
+import { Entypo } from "@expo/vector-icons";
 
 const Main = () => (
   <MainContainer>
     <Map />
     <Header />
-    <BottomSheet
-      snapPoints={[600, 300, 50]}
-      renderContent={() => (
-        <Surface style={{ width: "100%", height: "100%" }}>
-          <Text>Hello</Text>
-        </Surface>
-      )}
-      renderHeader={() => (
-        <Surface style={{ width: "100%" }}>
-          <Text>Hello</Text>
-        </Surface>
-      )}
-      initialSnap={2}
-    />
+    <BottomContent />
   </MainContainer>
 );
 
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,2 +1,3 @@
 export * from "./Map";
 export * from "./Header";
+export * from "./BottomContent";
