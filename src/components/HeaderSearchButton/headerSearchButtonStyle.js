3e87c12133bbc6cd5103483d12260039430879cc
Suyesh Bhandari <suyeshb@gmail.com>
2019-04-02 16:02:22 -0400

    .

--- src/components/HeaderSearchButton/headerSearchButtonStyle.js
+++ src/components/HeaderSearchButton/headerSearchButtonStyle.js
@@ -4,6 +4,20 @@ export const style = StyleSheet.create({
   headerSearchButton: {
     flex: 1,
     marginHorizontal: 15,
-    borderRadius: 50
+    height: 50
+  },
+  headerSearchButtonSurface: {
+    elevation: 3,
+    flex: 1,
+    borderRadius: 50,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingLeft: 20
+  },
+  text: {
+    fontSize: 18,
+    marginLeft: 15,
+    color: "grey"
   }
 });
--- src/components/HeaderSearchButton/index.js
+++ src/components/HeaderSearchButton/index.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { Searchbar } from "react-native-paper";
+import { TouchableOpacity } from "react-native";
+import { Surface, Text } from "react-native-paper";
+import { MaterialIcons } from "@expo/vector-icons";
 import { style } from "./headerSearchButtonStyle";
 
 const HeaderSearchButton = () => (
-  <Searchbar placeholder="Search" style={style.headerSearchButton} />
+  <TouchableOpacity style={style.headerSearchButton}>
+    <Surface style={style.headerSearchButtonSurface}>
+      <MaterialIcons name="search" size={24} color="grey" />
+      <Text style={style.text}>Search...</Text>
+    </Surface>
+  </TouchableOpacity>
 );
 
 export { HeaderSearchButton };
